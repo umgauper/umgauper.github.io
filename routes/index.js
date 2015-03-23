4b5ba5b4c7d170095e61c68a9f501263aeaf4fa8
Una Gauper <umgauper@gmail.com>
2015-03-23 22:56:53 +0700

    added bootstrap files, personalized banner image, updated route to load html file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+var html_dir = './public/html/';
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(html_dir + 'index.html');
 });
 
 module.exports = router;
