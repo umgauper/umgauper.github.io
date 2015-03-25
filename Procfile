0d89eeae174e4007f2d09fbb476483cb1f07d17a
Una Gauper <umgauper@gmail.com>
2015-03-25 16:24:37 +0700

    test

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-www: node bin/www
+www: node bin/www.js
 
--- bin/www.js
+++ bin/www.js
@@ -7,3 +7,7 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
+app.get('/', function(req, res) {
+	res.send('hello world!');
+});
