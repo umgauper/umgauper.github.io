822c2f02396a928021830de01852edf65d0717ca
Una Gauper <umgauper@gmail.com>
2015-03-25 16:41:32 +0700

    changed www.js to see if it will make heroku open work

--- bin/www.js
+++ bin/www.js
@@ -4,9 +4,10 @@ var app = require('../app');
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
+app.listen(3000);
+//var server = app.listen(app.get('port'), function() {
+  //console.log('Express server listening on port ' + server.address().port);
+//});
 
 app.get('/', function(req, res) {
 	res.send('hello world!');
