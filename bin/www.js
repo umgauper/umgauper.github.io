0f7bb9f98dc921bdd0426135d7be28a9c277346e
Una Gauper <umgauper@gmail.com>
2015-03-25 16:44:58 +0700

    trying again

--- bin/www.js
+++ bin/www.js
@@ -2,10 +2,9 @@
 var debug = require('debug')('my-application');
 var app = require('../app');
 
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 
-app.listen(3000);
-//var server = app.listen(app.get('port'), function() {
+app.listen(process.env.PORT);//var server = app.listen(app.get('port'), function() {
   //console.log('Express server listening on port ' + server.address().port);
 //});
 
